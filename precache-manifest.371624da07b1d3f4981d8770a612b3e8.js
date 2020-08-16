a8065b9589236399f587066a74ec5ece08781730
Daniel Bass <danielbass123456@gmail.com>
2020-08-15 21:19:00 -0500

    daDAdasdasd

---
+++ precache-manifest.371624da07b1d3f4981d8770a612b3e8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9879b08f27745dfa8b",
+    "url": "/anime-guess/css/app.5c9713c3.css"
+  },
+  {
+    "revision": "94d95b6ee8dd40abe670",
+    "url": "/anime-guess/css/chunk-vendors.8d9c285c.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/anime-guess/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/anime-guess/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4396f2325ed1e02484b86fd86009df79",
+    "url": "/anime-guess/index.html"
+  },
+  {
+    "revision": "b46d18a834ebd797b167",
+    "url": "/anime-guess/js/about.b007acd2.js"
+  },
+  {
+    "revision": "5e9879b08f27745dfa8b",
+    "url": "/anime-guess/js/app.98b331dc.js"
+  },
+  {
+    "revision": "94d95b6ee8dd40abe670",
+    "url": "/anime-guess/js/chunk-vendors.3c1ce149.js"
+  },
+  {
+    "revision": "be8f0808693094693b38c89b8b82593f",
+    "url": "/anime-guess/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/anime-guess/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/anime-guess/precache-manifest.371624da07b1d3f4981d8770a612b3e8.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "anime-guess"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
